7060f13480b90ddd1bd471ea599f0ca4aa88ae1f
ripudaman <ripudaman@asynchain.com>
2023-05-16 19:16:53 +0530

    msg

--- jobsp/settings_server.py
+++ jobsp/settings_server.py
@@ -16,49 +16,6 @@ sentry_sdk.init(
 )
 
 
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": True,
-    "root": {
-        "level": "WARNING",
-        "handlers": ["sentry"],
-    },
-    "formatters": {
-        "verbose": {
-            "format": "%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s"
-        },
-    },
-    "handlers": {
-        "sentry": {
-            "level": "ERROR",
-            "class": "raven.contrib.django.raven_compat.handlers.SentryHandler",
-        },
-        "console": {
-            "level": "DEBUG",
-            "class": "logging.StreamHandler",
-            "formatter": "verbose",
-        },
-    },
-    "loggers": {
-        "django.db.backends": {
-            "level": "ERROR",
-            "handlers": ["console"],
-            "propagate": False,
-        },
-        "raven": {
-            "level": "DEBUG",
-            "handlers": ["console"],
-            "propagate": False,
-        },
-        "sentry.errors": {
-            "level": "DEBUG",
-            "handlers": ["console"],
-            "propagate": False,
-        },
-    },
-}
-
-
 GIT_BRANCH = "master"
 UWSGI_FILE_NAME = "jobs_uwsgi.ini"
 
