5d6f4dc2586f3fafe4a0f12a9e249060c3dc6048
ripudaman <ripudaman@asynchain.com>
2023-05-16 19:37:50 +0530

    msg

--- jobsp/settings_server.py
+++ jobsp/settings_server.py
@@ -19,17 +19,17 @@ sentry_sdk.init(
 GIT_BRANCH = "master"
 UWSGI_FILE_NAME = "jobs_uwsgi.ini"
 
-AWS_STORAGE_BUCKET_NAME = AWS_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
-AWS_DEFAULT_ACL = "public-read"
-S3_DOMAIN = AWS_S3_CUSTOM_DOMAIN = str(AWS_BUCKET_NAME) + ".s3.amazonaws.com"
+# AWS_STORAGE_BUCKET_NAME = AWS_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+# AWS_DEFAULT_ACL = "public-read"
+# S3_DOMAIN = AWS_S3_CUSTOM_DOMAIN = str(AWS_BUCKET_NAME) + ".s3.amazonaws.com"
 
 LOGO = "https://%s/logo.png" % (S3_DOMAIN)
 
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-DEFAULT_S3_PATH = "media"
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-STATIC_S3_PATH = "static"
-COMPRESS_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+# DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+# DEFAULT_S3_PATH = "media"
+# STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+# STATIC_S3_PATH = "static"
+# COMPRESS_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 AWS_HEADERS = {
     "Expires": "Sun, 15 June 2020 20:00:00 GMT",
